d872979db9540f9f18c2ad14ebeb2adcc13171af
aurelien <aurelien@duboc.xyz>
2020-03-30 07:04:16 +0200

    Increase performance sendin payloads using async and aiohttp

--- upload/send_data.py
+++ upload/send_data.py
@@ -1,8 +1,11 @@
+import asyncio
+import aiohttp
 import numpy as np
 from io import BytesIO
 from PIL import Image
 import requests
 import time
+from typing import List
 
 
 def get_proof():
@@ -24,7 +27,7 @@ def get_tab_from_image():
     with open('image', 'wb') as f:
         f.write(r.content)
     all_tab = np.array(Image.open(BytesIO(r.content)))
-    return all_tab[-25-10:-10, 30:30+25].tolist()  # bottom left corner
+    return all_tab.tolist()  # bottom left corner
 
 
 def get_tab_from_custom_image():
@@ -46,15 +49,26 @@ def update_pixel(x: int, y: int, color: str, proof: str):
     time.sleep(0.03)
 
 
-def main():
+async def update_pixel_async(url) -> None:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            print((await r.text(), url.split('?')[1]))
+
+
+def get_update_pixel_url(x: int, y: int, color: str, proof: str):
+    return f'http://137.74.47.86/setpixel?x={x}&y={y}&color={color}&proof={proof}'
+
+
+def main() -> List[str]:
     proofs = get_proof()
     count = int(open('count.txt', 'rb').read().decode())
     proofs = proofs[count:]  # ignore already used proofs
     server_tab = get_tab_from_image()
     custom_tab = get_tab_from_custom_image()
+    urls = []
     if len(proofs) == 0:
         print('Wait to compute more hashes')
-        return
+        return urls
     for i in range(len(server_tab)):
         for j in range(len(server_tab[0])):
             server_pixel = server_tab[i][j][:3]
@@ -63,11 +77,12 @@ def main():
                 continue  # ignore transparent pixel
             if server_pixel == custom_pixel:
                 continue  # do not update pixel
-            y = 100 - 25 - 10 + i
-            x = 30 + j
+            y = i
+            x = j
             custom_color = ''.join([hex(i)[2:].zfill(2) for i in custom_pixel])
             proof = proofs[0]
-            update_pixel(x, y, custom_color, proof)
+            urls.append(get_update_pixel_url(x, y, custom_color, proof))
+            #  update_pixel(x, y, custom_color, proof)
             count += 1
             with open('count.txt', 'wb') as f:
                 f.write(str(count).encode())
@@ -75,9 +90,15 @@ def main():
             proofs = proofs[1:]  # update available proofs
             if len(proofs) == 0:
                 print('Wait to compute more hashes')
-                return
+                return urls
+    return urls
 
 
 if __name__ == '__main__':
     while True:
-        main()
+        urls = main()
+        loop = asyncio.get_event_loop()
+        async_tasks = []
+        for url in urls:
+            async_tasks.append(loop.create_task(update_pixel_async(url)))
+        loop.run_until_complete(asyncio.gather(*async_tasks))
